7f003e150d6d0ec90ec974bdadf8507a464e32c6
banti-kumar9128 <vk9128697037@gmail.com>
2026-01-29 19:45:30 +0530

    Fix WhiteNoise module error

--- hello/settings.py
+++ hello/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_DIRS = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'Hello', 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'Hello/static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'media')
